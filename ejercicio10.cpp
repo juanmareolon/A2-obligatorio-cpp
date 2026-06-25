e041d6dc81e160b1881d153785d57da3f8fca750
Juanma <JR331598@fi365.ort.edu.uy>
2026-06-25 15:41:48 -0300

    feature: ejercicio 10

--- ejercicio10.cpp
+++ ejercicio10.cpp
@@ -4,115 +4,153 @@
 
 using namespace std;
 
-
-// todas las cartas deben ser utilizadas exactamente una vez.
-
-bool esSolucion(int valorActual, int valorObjetivo){
-    return valorActual == valorObjetivo; 
+bool esSolucion(double* arrayCartas, int largoArray, double valorObjetivo){
+    if(largoArray == 1 && arrayCartas[0] == valorObjetivo) return true;
+    return false;
 }
 
-bool puedoAplicarMovimiento(double valorActual, int pos, int largoArray, char operador, double* arrayCartas){
-    if(pos >= largoArray) return false;
-    double resultadoOperacion;
+void hacerCalculo(double primerNro, double segundoNro, double& resultadoOperacion, char operador){
     switch (operador)
     {
     case '+':
-        resultadoOperacion = valorActual + arrayCartas[pos];
+        resultadoOperacion = primerNro + segundoNro;
         break;
     case '-':
-        resultadoOperacion = valorActual - arrayCartas[pos]; 
+        resultadoOperacion = primerNro - segundoNro; 
         break;
     case '*':
-        resultadoOperacion = valorActual * arrayCartas[pos]; 
+        resultadoOperacion = primerNro * segundoNro; 
         break;
     case '/':
-        resultadoOperacion = valorActual / arrayCartas[pos];
+        resultadoOperacion = primerNro / segundoNro;
     }
+}
 
+bool puedoAplicarMovimiento(int i, int j, double* arrayCartas, int largoArray, char operador){
+    double primerNro = arrayCartas[i];
+    double segundoNro = arrayCartas[j];
+    double resultadoOperacion;
+    hacerCalculo(primerNro, segundoNro, resultadoOperacion, operador);
     if(resultadoOperacion < 0)return false;
     else return resultadoOperacion == (int)resultadoOperacion;
 }
 
-void aplicarMovimiento(double &valorActual, int pos, double* arrayCartas, char operacion){
-    switch (operador)
+double* copiarArray(double* arrayCartas, double* nuevoArray, int posI, int posJ, double resultadoOperacion, int largoArray){
+    int k = 0;
+    for (int p = 0; p < largoArray; p++)
     {
-    case '+':
-        valorActual += arrayCartas[pos];
-        break;
-    case '-':
-        valorActual -= arrayCartas[pos]; 
-        break;
-    case '*':
-        valorActual *= arrayCartas[pos]; 
-        break;
-    case '/':
-        valorActual /= arrayCartas[pos];
+        if (p == posI)
+        {
+            nuevoArray[k++] = resultadoOperacion;
+        }
+        else if (p != posJ)
+        {
+            nuevoArray[k++] = arrayCartas[p];
+        }
     }
+    return nuevoArray;
 }
 
-
-void deshacerMovimiento(double &valorActual, int pos, double* arrayCartas, char operacion){
-    switch (operador)
-    {
-    case '+':
-        valorActual -= arrayCartas[pos];
-        break;
-    case '-':
-        valorActual += arrayCartas[pos]; 
-        break;
-    case '*':
-        valorActual /= arrayCartas[pos]; 
-        break;
-    case '/':
-        valorActual *= arrayCartas[pos];
-    }
+void aplicarMovimiento(int i, int j, double*& arrayCartas, int& largoArray, char operador){
+    double primerNro = arrayCartas[i];
+    double segundoNro = arrayCartas[j];
+    double* nuevoArray = new double[largoArray - 1];
+    double resultadoOperacion;
+    hacerCalculo(primerNro, segundoNro, resultadoOperacion, operador);
+    nuevoArray = copiarArray(arrayCartas, nuevoArray, i, j, resultadoOperacion, largoArray);
+    arrayCartas = nuevoArray;
+    largoArray--;
 }
 
+void deshacerMovimiento(double* arrayAnterior, int largoAnterior, double*& arrayCartas, int& largoArray){
+    delete[] arrayCartas;
+    arrayCartas = arrayAnterior;
+    largoArray = largoAnterior;
+}
 
-void BT_Decision(double* arrayCartas, int largoArray, int pos, double valorActual, bool &exito, double valorObjetivo){
-    if(!exito){
-        if(esSolucion(valorActual, valorObjetivo)){
-            cout << 1;
+void BT_Decision(double *arrayCartas, int largoArray, bool &exito, double valorObjetivo)
+{
+    if (!exito)
+    {
+        if (esSolucion(arrayCartas, largoArray, valorObjetivo))
+        {
+            cout << 1 << endl;
             exito = true;
         }
-        else{
-            for (int i = 0; i < 4; i++)
+        else
+        {
+            for (int i = 0; i < largoArray; i++)
             {
-                switch (i)
+                for (int j = i + 1; j < largoArray; j++)
                 {
-                case 0:
-                    if (puedoAplicarMovimiento(valorActual, pos + 1, largoArray,'+', arrayCartas)){
-                    aplicarMovimiento(valorActual, pos + 1, arrayCartas, '+');
-                    BT_Decision(arrayCartas, largoArray, pos + 1, valorActual, exito, valorObjetivo);
-                    deshacerMovimiento(valorActual, pos + 1, arrayCartas, '+');
+                    for (int operacion = 0; operacion < 6; operacion++)
+                    {
+                        double* arrayAnterior = arrayCartas;
+                        int largoAnterior = largoArray;
+                        switch (operacion)
+                        {
+                        case 0:
+                            if(puedoAplicarMovimiento(i, j, arrayCartas, largoArray,'+')){
+                                aplicarMovimiento(i, j, arrayCartas, largoArray, '+');
+                                BT_Decision(arrayCartas, largoArray, exito, valorObjetivo);
+                                deshacerMovimiento(arrayAnterior, largoAnterior, arrayCartas, largoArray);
+
+                            }
+                            break;
+                        case 1:
+                            if(puedoAplicarMovimiento(i, j, arrayCartas, largoArray, '-')){
+                                aplicarMovimiento(i, j, arrayCartas, largoArray, '-');
+                                BT_Decision(arrayCartas, largoArray, exito, valorObjetivo);
+                                deshacerMovimiento(arrayAnterior, largoAnterior, arrayCartas, largoArray);
+
+                            }
+                            break;
+                        case 2:
+                            if(puedoAplicarMovimiento(i, j, arrayCartas, largoArray, '*')){
+                                aplicarMovimiento(i, j, arrayCartas, largoArray, '*');
+                                BT_Decision(arrayCartas, largoArray, exito, valorObjetivo);
+                                deshacerMovimiento(arrayAnterior, largoAnterior, arrayCartas, largoArray);
+
+                            }
+                            break;
+                        case 3:
+                            if(puedoAplicarMovimiento(i, j, arrayCartas, largoArray, '/')){
+                                aplicarMovimiento(i, j, arrayCartas, largoArray, '/');
+                                BT_Decision(arrayCartas, largoArray, exito, valorObjetivo);
+                                deshacerMovimiento(arrayAnterior, largoAnterior, arrayCartas, largoArray);
+
+                            }
+                            break;
+                        case 4:
+                            if(puedoAplicarMovimiento(j, i, arrayCartas, largoArray, '/')){
+                                aplicarMovimiento(j, i, arrayCartas, largoArray, '/');
+                                BT_Decision(arrayCartas, largoArray, exito, valorObjetivo);
+                                deshacerMovimiento(arrayAnterior, largoAnterior, arrayCartas, largoArray);
+
+                            }
+                            break;
+                        case 5:
+                            if(puedoAplicarMovimiento(j, i, arrayCartas, largoArray, '-')){
+                                aplicarMovimiento(j, i, arrayCartas, largoArray, '-');
+                                BT_Decision(arrayCartas, largoArray, exito, valorObjetivo);
+                                deshacerMovimiento(arrayAnterior, largoAnterior, arrayCartas, largoArray);
+                            }
+                            break;
+                        }
                     }
-                    break;
-                
-                case 1:
-                    /* code */
-                    break;
-
-                case 2:
-                    /* code */
-                    break;
-
-                case 3:
-                    /* code */
-                    break;
                 }
             }
         }
     }
 }
 
-
-
-int main(){ 
+int main()
+{
     int n; // cantidadCartas
-    double* valoresCartas = new double[n];
-    double valorObjetivo;
     cin >> n;
-    for (int i = 0; i <= n; i++)
+    double *valoresCartas = new double[n];
+    double valorObjetivo;
+    for (int i = 0; i < n; i++)
     {
         double dato;
         cin >> dato;
@@ -120,8 +158,7 @@ int main(){
     }
     cin >> valorObjetivo;
     bool exito = false;
-    BT_Decision(valoresCartas, n, 0, 0,exito,valorObjetivo)
-        
-    if(!exito) cout << 0;
+    BT_Decision(valoresCartas, n, exito, valorObjetivo);
+    if (!exito) cout<< 0 << endl;
     return 0;
 }
